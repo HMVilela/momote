db9a718bf97575d8afdd055a605da0a672aced4f
Henrique Vilela <Henrique Vilela>
2015-12-01 14:39:19 -0200

    atualizado paginas separadas

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,8 @@
+$("#pop").on("click", function() {
+   $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+   $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+
 /*!
  * Bootstrap v3.3.4 (http://getbootstrap.com)
  * Copyright 2011-2015 Twitter, Inc.

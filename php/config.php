f07af9429cb48cf1b9a1950d756adaf0e1ef4f22
Henrique Vilela <henrique.dev01@gmail.com>
2015-12-03 00:26:33 -0200

    config empt

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php 
 
- $HOST = "mysql.casaroli.com.br";//"mysql.casaroli.com.br";
- $USUARIO = "momote"; //"momote";
- $SENHA = "brownbrown!@88543"; //"brownbrown!@88543";
- $NOMEDOBD = "momote";
+ $HOST = "localhost";//"mysql.casaroli.com.br";
+ $USUARIO = "root"; //"momote";
+ $SENHA = ""; //"brownbrown!@88543";
+ $NOMEDOBD = "db";
  $EMAIL_DE_DESTINO = "contato@momote.io";
 ?>
